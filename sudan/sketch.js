99d74089bfc3ea97c761e773c3378573ce4e0a97
Ciaron Linstead <ciaron.linstead@gmail.com>
2021-09-10 19:27:00 +0200

    working! (Extra control points, 0,1,2)

--- sudan/sketch.js
+++ sudan/sketch.js
@@ -31,7 +31,7 @@ function draw() {
   background(0);
 
   stroke('white');
-  strokeWeight(2)
+  strokeWeight(8)
 
   beginShape();
 
@@ -42,11 +42,11 @@ function draw() {
     v1 = va[i]
     v2 = va[i+1]
   
-    for (let i=0; i<=1.0; i+=0.1) {
+    for (let i=0; i<=1.0; i+=0.125) {
         vtmp = p5.Vector.lerp(v1, v2, i)
         
-        vtmp.x += random(-5,5)
-        vtmp.y += random(-5,5)
+        vtmp.x += random(-11,11)
+        vtmp.y += random(-11,11)
         vxa.push(vtmp);    
     }
     vxa.pop()
@@ -55,28 +55,33 @@ function draw() {
   }
   //vxa.push(va[va.length-1])
   
-  curveVertex(va[0].x, va[0].y);
+  //curveVertex(va[0].x, va[0].y);
 
-  // for (let i=0; i<(vxa.length); i+=1) {
-  //   curveVertex(vxa[i].x, vxa[i].y);
-  //   ellipse(vxa[i].x, vxa[i].y, 20, 20);
-  // }
+  for (let i=0; i<(vxa.length); i+=1) {
+     curveVertex(vxa[i].x, vxa[i].y);
+     //ellipse(vxa[i].x, vxa[i].y, 20, 20);
+   }
 
-  for (let i=0; i<(va.length); i+=1) {
-    curveVertex(va[i].x, va[i].y);
-    ellipse(va[i].x, va[i].y, 20, 20);
-  }
+  //for (let i=0; i<(va.length); i+=1) {
+  //  curveVertex(va[i].x, va[i].y);
+  //  ellipse(va[i].x, va[i].y, 20, 20);
+ // }
 
   //curveVertex(va[0].x, va[0].y);
   //curveVertex(vxa[vxa.length-1].x, vxa[vxa.length-1].y);
   //curveVertex(va[va.length-1].x, va[va.length-1].y);
-  
-  curveVertex(va[va.length-1].x, va[va.length-1].y);
+  //curveVertex(va[0].x, va[0].y);
+  //curveVertex(va[1].x, va[1].y);
+  //curveVertex(va[2].x, va[2].y);
+  //curveVertex(va[va.length-1].x, va[va.length-1].y);
   //curveVertex(va[1].x, va[1].y);
   //curveVertex(vxa[vxa.length-1].x, vxa[vxa.length-1].y);
   //curveVertex(va[va.length-1].x, va[va.length-1].y);
 
-  //curveVertex(va[0].x, va[0].y);
+  
+  curveVertex(vxa[0].x, vxa[0].y);
+  curveVertex(vxa[1].x, vxa[1].y);
+  curveVertex(vxa[2].x, vxa[2].y);
   //curveVertex(va[0].x, va[0].y);
   //curveVertex(va[va.length-1].x, va[va.length-1].y);
 
