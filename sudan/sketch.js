fba21b65daff8cc5d902d507ed4f55b103ad39a0
Ciaron Linstead <ciaron.linstead@gmail.com>
2021-09-10 19:54:22 +0200

    changed randomness

--- sudan/sketch.js
+++ sudan/sketch.js
@@ -1,91 +1,84 @@
 let va; // Vector array
-let vxa; // curveVertex array...
 
 let v1;
 let v2;
 let vtmp;
 
-let step=5; // percent stepsize
+let bg='white'
+let fg='black'
 
 function setup() {
   let c = createCanvas(800, 800);
   va = []
-  vxa = []
-
-  //v1=createVector(700,100);
-  //v2=createVector(700,700);
-  
-  noFill();
-  strokeWeight(2);
 
   // a rectangle
   va.push(createVector(100, 100));
   va.push(createVector(700, 100));
   va.push(createVector(700, 700));
   va.push(createVector(100, 700));
-  va.push(createVector(100, 100));
-    
+  va.push(createVector(100, 100)); // back to the start
+
+  // square with a piece missing
+  // va.push(createVector(100, 350));
+  // va.push(createVector(350, 350));
+  // va.push(createVector(350, 100));
+  // va.push(createVector(700, 100));
+  // va.push(createVector(700, 700));
+  // va.push(createVector(100, 700));
+  // va.push(createVector(100, 350));
+  
+  // triangle
+  // va.push(createVector(400, 150));
+  // va.push(createVector(150, 650));
+  // va.push(createVector(650, 650));
+  // va.push(createVector(400, 250));
+  
 }
 
 function draw() {
-  background(0);
+  background(bg);
 
-  stroke('white');
+  drawShape(va);
+  
+  noLoop();
+}
+
+function drawShape(va){
+  /* takes an array of vectors and decomposes into an array of curveVertices */
+
+  let vxa = []
+  stroke(fg);
   strokeWeight(8)
-
+  
   beginShape();
 
-  //curveVertex(va[0].x, va[0].y);
-  //vxa.push(va[0])
   for (let i=0; i<(va.length-1); i+=1) {
     
     v1 = va[i]
     v2 = va[i+1]
   
-    for (let i=0; i<=1.0; i+=0.125) {
+    let step = 0.075;//map(v1.dist(v2), 0, width, 0.125, 0.275)
+    for (let i=0; i<=1.0; i+=step) {
         vtmp = p5.Vector.lerp(v1, v2, i)
         
-        vtmp.x += random(-11,11)
-        vtmp.y += random(-11,11)
+        vtmp.x += floor(random(-5,6))
+        vtmp.y += floor(random(-5,6))
         vxa.push(vtmp);    
     }
     vxa.pop()
-    //curveVertex(va[va.length-1].x, va[va.length-1].y);
-    //curveVertex(v2.x, v2.y);
   }
-  //vxa.push(va[va.length-1])
-  
-  //curveVertex(va[0].x, va[0].y);
+
 
   for (let i=0; i<(vxa.length); i+=1) {
-     curveVertex(vxa[i].x, vxa[i].y);
-     //ellipse(vxa[i].x, vxa[i].y, 20, 20);
-   }
+    curveVertex(vxa[i].x, vxa[i].y);
+    //ellipse(vxa[i].x, vxa[i].y, 20, 20);
+  }
 
-  //for (let i=0; i<(va.length); i+=1) {
-  //  curveVertex(va[i].x, va[i].y);
-  //  ellipse(va[i].x, va[i].y, 20, 20);
- // }
-
-  //curveVertex(va[0].x, va[0].y);
-  //curveVertex(vxa[vxa.length-1].x, vxa[vxa.length-1].y);
-  //curveVertex(va[va.length-1].x, va[va.length-1].y);
-  //curveVertex(va[0].x, va[0].y);
-  //curveVertex(va[1].x, va[1].y);
-  //curveVertex(va[2].x, va[2].y);
-  //curveVertex(va[va.length-1].x, va[va.length-1].y);
-  //curveVertex(va[1].x, va[1].y);
-  //curveVertex(vxa[vxa.length-1].x, vxa[vxa.length-1].y);
-  //curveVertex(va[va.length-1].x, va[va.length-1].y);
-
-  
+  // close the curve
   curveVertex(vxa[0].x, vxa[0].y);
   curveVertex(vxa[1].x, vxa[1].y);
   curveVertex(vxa[2].x, vxa[2].y);
-  //curveVertex(va[0].x, va[0].y);
-  //curveVertex(va[va.length-1].x, va[va.length-1].y);
 
   endShape();
-  
-  noLoop();
+
 }
