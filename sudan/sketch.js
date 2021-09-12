9e71f41b81274012ad040be2945088f0c70c86bc
Ciaron Linstead <ciaron.linstead@gmail.com>
2021-09-12 19:48:54 +0200

    tests

--- sudan/sketch.js
+++ sudan/sketch.js
@@ -4,7 +4,7 @@ let v1;
 let v2;
 let vtmp;
 
-let bg='white'
+let bg='grey'
 let fg='black'
 
 function setup() {
@@ -35,22 +35,24 @@ function setup() {
   
 }
 
-function getShape() {
+function getShape(x,y,w,h) {
   va = []
 
   // a rectangle
-  let min = -20;
-  let max = 20;
+  let min = -w/25;
+  let max = w/25;
   p = random(min, max); q=random(min, max);
-  va.push(createVector(100+p, 100+q));
+  let initx=x+p;
+  let inity=y+q;
+  va.push(createVector(x+p, y+q));
   p = random(min, max); q=random(min, max);
-  va.push(createVector(700+p, 100+q));
+  va.push(createVector(x+w+p, y+q));
   p = random(min, max); q=random(min, max);
-  va.push(createVector(700+p, 700+q));
+  va.push(createVector(x+w+p, y+h+q));
   p = random(min, max); q=random(min, max);
-  va.push(createVector(100+p, 700+q));
-  p = random(min, max); q=random(min, max);
-  va.push(createVector(100+p, 100+q)); // back to the start
+  va.push(createVector(x+p, y+h+q));
+  //p = random(min, max); q=random(min, max);
+  va.push(createVector(initx, inity)); // back to the start
 
   return va;
 
@@ -59,18 +61,20 @@ function getShape() {
 function draw() {
   background(bg);
 
-  // for (let y=0; y<height-100; y+=random(100,200)) {
-  //   for (let x=0; x<width-100; x+=random(100,200)){
+  ////for (let y=100; y<height-100; y+=random(100,200)) {
+  ////  for (let x=100; x<width-100; x+=random(100,200)) {
+  // for (let y=height/10; y<9*height/10; y+=random(height/10,2*height/10)) {
+  //   for (let x=width/10; x<9*width/10; x+=random(width/10,2*width/10)) {    
   //     push();
-  //     translate(x+10, y+10);
+  //     translate(x, y);
   //     //scale(0.1);
-  //     shp = getShape();
+  //     shp = getShape(0,0,width/10,height/10);
   //     drawShape(shp);
   //     pop();
   //   }
   // }
-  va=getShape();
-  drawShape(va);
+  va=getShape(100,100,600,600);
+  drawShape2(va);
   
   noLoop();
 }
@@ -89,12 +93,53 @@ function drawShape(va){
     v1 = va[i]
     v2 = va[i+1]
   
-    let step = 0.075;//map(v1.dist(v2), 0, width, 0.125, 0.275)
+    let step = 0.175;  //map(v1.dist(v2), 0, width, 0.125, 0.275)
     for (let i=0; i<=1.0; i+=step) {
         vtmp = p5.Vector.lerp(v1, v2, i)
         
         vtmp.x += floor(random(-5,6))
         vtmp.y += floor(random(-5,6))
+        vxa.push(vtmp);    
+    }
+    vxa.pop()
+  }
+
+
+  for (let i=0; i<(vxa.length); i+=1) {
+    curveVertex(vxa[i].x, vxa[i].y);
+    ellipse(vxa[i].x, vxa[i].y, 20, 20);
+  }
+
+  // close the curve
+  curveVertex(vxa[0].x, vxa[0].y);
+  curveVertex(vxa[1].x, vxa[1].y);
+  curveVertex(vxa[2].x, vxa[2].y);
+   noFill();
+  endShape();
+
+}
+
+
+function drawShape2(va){
+  /* takes an array of vectors and decomposes into an array of curveVertices */
+
+  let vxa = []
+  stroke(fg);
+  strokeWeight(8)
+  
+  beginShape();
+
+  for (let i=0; i<(va.length-1); i+=1) {
+    
+    v1 = va[i]
+    v2 = va[i+1]
+  
+    let step = 0.075; 
+    for (let i=0; i<=1.0; i+=step) {
+        vtmp = p5.Vector.lerp(v1, v2, i)
+        
+        vtmp.x += floor(random(-2,3))
+        vtmp.y += floor(random(-2,3))
         vxa.push(vtmp);    
     }
     vxa.pop()
@@ -110,7 +155,7 @@ function drawShape(va){
   curveVertex(vxa[0].x, vxa[0].y);
   curveVertex(vxa[1].x, vxa[1].y);
   curveVertex(vxa[2].x, vxa[2].y);
-
+   noFill();
   endShape();
 
 }
